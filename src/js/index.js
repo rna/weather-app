68ea60eb4e44ef719005b7466ae6f5212ceb112b
Ramesh <rameshnaidu.allu@gmail.com>
2020-09-03 15:42:43 +0530

    fix typo in api env variable

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 import '../assets/styles/main.scss';
 
-const apiKey = process.env.WEATHER_APP_apiKey;
+const apiKey = process.env.WEATHER_APP_API_KEY;
 
 document.getElementById('searchButton').addEventListener('click', () => {
   const place = document.getElementById('search').value;
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=${apiKey}`, { mode: 'cors' })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.name);
+      console.log(data);
     })
     .catch((err) => {
       console.log(err);
