a984613682a84bbb654cf2e62d9a5096f6add7ff
Ramesh <rameshnaidu.allu@gmail.com>
2020-09-04 20:01:20 +0530

    refactor code using Object destructuring

--- src/js/apiData.js
+++ src/js/apiData.js
@@ -2,14 +2,15 @@
 import gifObject from './gifObject';
 
 const addApiData = (data) => {
+  const { name, weather, main } = data;
   const cityName = document.getElementById('city');
-  const weather = document.getElementById('weather');
+  const weatherInfo = document.getElementById('weather');
   const temp = document.getElementById('temp');
-  document.body.style.background = `url(${gifObject[data.weather[0].icon]}) no-repeat`;
+  document.body.style.background = `url(${gifObject[weather[0].icon]}) no-repeat`;
   document.body.style.backgroundSize = 'cover';
-  cityName.innerHTML = data.name;
-  weather.innerHTML = data.weather[0].main;
-  temp.innerHTML = Math.round(data.main.temp);
+  cityName.innerHTML = name;
+  weatherInfo.innerHTML = weather[0].main;
+  temp.innerHTML = Math.round(main.temp);
 };
 
 export default addApiData;
